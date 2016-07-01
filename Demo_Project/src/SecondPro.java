b5b5300cecec62d267b400490dc6724f82190c48
Jivesh <jiveshkadam26@gmail.com>
2016-07-01 16:18:54 +0530

    Second Commit

---
+++ Demo_Project/src/SecondPro.java
@@ -0,0 +1,9 @@
+
+public class SecondPro {
+	public static void main(String args[]){
+		System.out.println("Second Jivesh");
+		System.out.println("Second Push");
+		System.out.println("Second Pull");
+	}
+
+}
